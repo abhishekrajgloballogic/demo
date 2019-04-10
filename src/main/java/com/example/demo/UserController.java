5cd0424e0911e123fa925ed7a48da47c9975f234
Abhishek Raj <abhishek.raj1@globallogic.com>
2019-04-10 14:29:46 +0530

    aaa

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -1,6 +1,5 @@
 package com.example.demo;
 
-import jdk.nashorn.internal.objects.annotations.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
